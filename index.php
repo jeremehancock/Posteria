2a3f956ea5099f4792c9882d1090f8d68fdb07e3
Jereme <michael@jeremehancock.com>
2025-03-01 22:51:42 -0600

    fixed send to plex svg

--- index.php
+++ index.php
@@ -2818,11 +2818,11 @@ $pageImages = array_slice($filteredImages, $startIndex, $config['imagesPerPage']
     <button class="overlay-action-button send-to-plex-btn" 
             data-filename="<?php echo htmlspecialchars($image['filename']); ?>"
             data-dirname="<?php echo htmlspecialchars($image['directory']); ?>">
-            <svg class="image-action-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-                <polyline points="17 8 12 3 7 8"></polyline>
-                <line x1="12" y1="3" x2="12" y2="15"></line>
-            </svg>
+				<svg class="image-action-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+				    <path d="M9 3h-4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h4"></path>
+				    <polyline points="16 7 21 12 16 17"></polyline>
+				    <line x1="21" y1="12" x2="9" y2="12"></line>
+				</svg>
         Send to Plex
     </button>
     
@@ -7018,11 +7018,11 @@ $pageImages = array_slice($filteredImages, $startIndex, $config['imagesPerPage']
 		                        sendToPlexButton.setAttribute('data-filename', filenameValue);
 		                        sendToPlexButton.setAttribute('data-dirname', directoryValue);
 		                        sendToPlexButton.innerHTML = `
-		                            <svg class="image-action-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-		                                <path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"></path>
-		                                <polyline points="16 6 12 2 8 6"></polyline>
-		                                <line x1="12" y1="2" x2="12" y2="15"></line>
-		                            </svg>
+									<svg class="image-action-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+										<path d="M9 3h-4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h4"></path>
+										<polyline points="16 7 21 12 16 17"></polyline>
+										<line x1="21" y1="12" x2="9" y2="12"></line>
+									</svg>
 		                            Send to Plex
 		                        `;
 		                        
