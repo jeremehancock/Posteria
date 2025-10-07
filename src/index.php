dd4ef76f30ed11c11c2cdffb45966be3f59e0825
Jereme <michael@jeremehancock.com>
2025-10-07 07:28:08 -0500

    fixed poster wall link

--- src/index.php
+++ src/index.php
@@ -3420,8 +3420,8 @@ $pageImages = array_slice($filteredImages, $startIndex, $config['imagesPerPage']
 						</button>
 					</div>
 					<div>
-						<a href="./poster-wall" class="poster-wall-button tooltip-trigger" data-tooltip="Poster Wall"
-							target="_blank">
+						<a href="<?php echo $config['siteUrl']; ?>poster-wall/"
+							class="poster-wall-button tooltip-trigger" data-tooltip="Poster Wall" target="_blank">
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
 								fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
 								stroke-linejoin="round">
